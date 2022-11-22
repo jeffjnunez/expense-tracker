b8cda883285d2cdd0fba32d8965ae22798460a9f
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-11-21 17:54:54 -0800

    038 - styled date data, moved to new component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ id, title, amount, date }) => {
+
     return (
         <div className='expense-item'>
-            <div>{date.toISOString()}</div>
+            <ExpenseDate date={date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
