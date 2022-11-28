e146b1a940a29b6a0b5f866954de5fdc41ed0846
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-11-27 16:33:41 -0800

    059 - assignment 2 - ExpensesFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,23 @@
-import ExpenseItem from './ExpenseItem';
+import { useState } from 'react';
+
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = ({ expenses }) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = value => {
+        setFilteredYear(value);
+    };
+
     return (
         <Card className='expenses'>
+            <ExpensesFilter
+                onFilterChange={filterChangeHandler}
+                selected={filteredYear}
+            />
             {expenses.map(expItem => (
                 <ExpenseItem
                     key={expItem.id}
