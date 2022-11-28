0cfe8a338d8d07c40ac5f9f222aa2427947afd60
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-11-27 16:41:02 -0800

    060 - controlled vs uncontrolled components ExpensesFilter is an example of a controlled component because it is controlled by the two-way binding. stateless aka presentational vs stateful components. aka dumb vs smart components.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
-import { useState } from 'react';
-
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ id, title, amount, date }) => {
-    const [itemTitle, setItemTitle] = useState(title);
-
-    const clickHandler = () => {
-        setItemTitle('Updated!');
-    };
-
     return (
         <Card className='expense-item'>
             <ExpenseDate date={date} />
             <div className='expense-item__description'>
-                <h2>{itemTitle}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
